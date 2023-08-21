f879a708416a2b0e5744cdca3538d0cb84978a4e
arahangua <arahangua@gmail.com>
2023-08-22 00:30:52 +0200

    fixed subprocess_handler log notation

--- subprocess_handler.py
+++ subprocess_handler.py
@@ -2,10 +2,14 @@ import os,sys
 import subprocess
 import numpy as np
 # read in batch_job.txt file
-process_n = 16
+process_n = 50
 
 with open('./batch_job.txt', 'r') as infile:
      lines = [line.strip() for line in infile.readlines()]
+
+for ii, line in enumerate(lines):
+    lines[ii] = line + ' -j ' + str(ii%process_n)
+   
 
 n_chunk = int(np.ceil(len(lines)/process_n))
 curr_point = 0
